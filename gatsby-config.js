3770f5ed1942961e8c305b848f470d36ab5f7dd9
DacD <dacdao01@gmail.com>
2019-04-16 14:17:24 -0400

    adding google analytics to config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,12 @@
 module.exports = {
+
   siteMetadata: {
     title: "Dac Dao | Software Engineer",
     author: "Dac Dao",
     description: "A Gatsby.js portfolio that is created with love."
   },
+
+
   plugins: [
     'gatsby-plugin-react-helmet',
     {
@@ -17,6 +20,17 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
+    },
+
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-138454554-1",
+        head: false,
+        anonymize: true,
+      },
+
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
