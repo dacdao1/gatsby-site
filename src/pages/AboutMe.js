cce244080b1ca0185e2ea6e3caaa5e58218ef943
DacD <dacdao01@gmail.com>
2019-05-02 23:51:35 -0400

    update the About Me page

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -27,15 +27,18 @@ class AboutMe extends React.Component {
                         <header className="major">
                             <h2>More information about myself and what my past experiences. </h2>
                         </header>
-                        <p><strong>Computer Science major graduate (GSU class of '18)</strong> that have built software with <strong>C, x86 (Assembly), Javascript (Vue, Nuxt, React, Electron and Angular) and Python.</strong> However, Java will always be my first love.<br/><br/>
+                        <p>
 
-I have <strong>experiences working for two different start-up company here in Atlanta</strong>. With one of the company, I was able to create a Python software that would data scraped different restaurant websites and Craiglist to retrieve contact information and develop new leads for the sales team. I also created a mock site for the other start-up company here in Atlanta, with using mostly HTML, CSS, and Javascript. I'm most proud of my Software Engineer class project. Our group apply the Agile methodology to sprint to the finish line and present the final product to our professor. We built a desktop application with HTML, CSS, and Javascript (Electronjs).<br/><br/>
+<strong>Computer Science Graduate (GSU class of '18)</strong> experienced in building software with <strong></strong>Java, C, x86 (Assembly), JavaScript (Vue, Nuxt, React, Electron and Angular) and Python.</strong> Even though I’ve gained experience in these other languages, Java will always be my first love.<br/><br/>
 
-I also was lucky enough to be a <strong>part of two different open source projects</strong>, <a href="https://github.com/Kentico/kentico.github.io">Kentico</a> and <a href="https://github.com/CiviWiki/OpenCiviWiki">OpenCiviWiki</a>, while the issues that I worked on for both open sources software were different but the result is gratifying. With Kentico, I edited the index.scss file to optimized all of the variables and mixin to be more organized and scalable. Moreover, with OpenCiviWiki, I used Docker to replicate the development environment on a personal desktop, and I mostly worked on the Edit profile exit button.<br/><br/>
+I have worked for <strong>two different start-up companies here in Atlanta</strong>. At one of these companies, I created a Python software that data scraped various restaurant websites and Craigslist to retrieve contact information and develop new leads for the sales team based off of this information. I also created a mock site for the other start-up company using mostly HTML, CSS, and JavaScript. Looking at all of my projects, I'm proudest of my Software Engineering class project. Our group applied the Agile methodology to build a desktop application with HTML, CSS, and JavaScript (ElectronJS). <br/><br/>
 
-My strength is understanding the concept of programming and problem-solving. I can learn a new programming language to help aid me to achieve my goal of solving a specific problem. My weakness would be not able to find the right answer right away; I still need a little bit of effort and time to works out all of the challenges. But I think that my strong suit, being very stubborn, that I will find the solution somehow.<br/><br/>
+I’ve also had the opportunity to work on <strong>two different open source projects,</strong> <a href="https://github.com/Kentico/kentico.github.io">Kentico</a> and <a href="https://github.com/CiviWiki/OpenCiviWiki">OpenCiviWiki</a>. With Kentico, I edited the index.scss file to optimize all the variables and mixin to increase organization and scalability. With OpenCiviWiki, I used Docker to replicate the development environment on a personal desktop, and I worked on the ‘Edit Profile’ exit button.
+<br/><br/>
 
-To find all of my works, codes, and project. Please feel free to click <a href="Portfolio">here</a> to go to my portfolio section or click on the Github icon below. I look forward to working with you.
+My strengths are understanding the concepts behind programming and then applying those concepts through excellent problem-solving. I’m an avid learning and can quickly learn new programming languages.<br/><br/>
+
+To find all of my works, codes, and project, please feel free to click <a href="Portfolio">here</a> to go to my portfolio section or click on the Github icon below. I look forward to working with you.
 
 </p>
 
