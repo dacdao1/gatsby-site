9fba87bb5552f4dcfc236827580e53a5fceb26ad
DacD <dacdao01@gmail.com>
2021-04-18 18:14:42 -0400

    update JS

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import '../assets/scss/main.scss'
-
 import Header from './Header'
 
 class Template extends React.Component {
     render() {
         const { children } = this.props
-
         return (
             <div>
                 <Header />
-
                 {children}
             </div>
         )
     }
 }
-
 export default Template
